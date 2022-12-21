398926f0581e637797b7ce26e8c531e7b28d4c24
Tidiane Diallo <diallotidiane014@gmail.com>
2022-12-21 15:43:24 +0000

    first commti

---
+++ index.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+interface Todo{
+    userId: number;
+    id:number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then(response =>{
+    //const todo = response.data
+    //----this is if we use the interface todo
+    const todo = response.data as Todo; 
+    
+    const userId = todo.userId;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    logTodo(userId,id,title,completed);
+});
+
+const logTodo = function(userId:number, id:number ,title:string , completed:boolean) {
+    console.log(`
+        userId: ${userId};
+        The to do with ID: ${id}
+        has a title of: ${title}
+        It is completed : ${completed}
+    `);
+}
